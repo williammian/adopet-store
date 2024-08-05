95430d5c23ee0122232669a5a6cc9e732ec25f13
William Mian <william_mian@yahoo.com.br>
2024-08-05 13:43:23 -0300

    @Async

--- src/main/java/br/com/wm/adopetstore/email/EmailPedidoRealizado.java
+++ src/main/java/br/com/wm/adopetstore/email/EmailPedidoRealizado.java
@@ -3,6 +3,7 @@ package br.com.wm.adopetstore.email;
 import br.com.wm.adopetstore.dto.PedidoDTO;
 import br.com.wm.adopetstore.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,10 +11,12 @@ public class EmailPedidoRealizado {
     @Autowired
     private EnviadorEmail enviador;
 
+    @Async("asyncExecutor")
     public void enviar(PedidoDTO dto, Usuario usuario){
         enviador.enviarEmail(
                 "Pedido efetuado com sucesso na Adopet Store",
                 usuario.getEmail(),
                 "Olá! " + "!\n\nSeu pedido foi registrado. Itens: \n" + dto.itens());
+        System.out.println("Thread do email: " + Thread.currentThread().getName());
     }
 }
